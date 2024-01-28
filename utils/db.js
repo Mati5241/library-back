7d93aa6ffb83e5ae73cbea554b155f053e05d5c2
Mati5241 <143203781+Mati5241@users.noreply.github.com>
2024-01-28 16:08:12 +0100

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,28 +1,11 @@
-import mysql from 'mysql2/promise';
+import {createPool} from "mysql2/promise";
+import {config} from "../config/config";
 
-
-
-export const pool = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    database: 'library',
-    decimalNumbers: true,
-    multipleStatements: true,
+export const pool = createPool({
+    host: config.dbHost,
+    user: config.dbUser,
+    database: config.dbDatabase,
+    // password: config.dbPassword,
     namedPlaceholders: true,
-
+    decimalNumbers: true,
 });
-
-//todo zmienic to przed wyslaniem na serwer
-
-
-// import {createPool} from "mysql2/promise";
-// import {config} from "../config/config";
-//
-// export const pool = createPool({
-//     host: config.dbHost,
-//     user: config.dbUser,
-//     database: config.dbDatabase,
-//     // password: config.dbPassword,
-//     namedPlaceholders: true,
-//     decimalNumbers: true,
-// });
